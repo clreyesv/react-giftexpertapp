835f4a4ec421c9cc198293e54a80f198f7ea3c43
Claudio Reyes Jederzeit <clreyesv@gmail.com>
2021-03-22 15:36:50 +0100

    getGif actualizado de http a https

--- src/helpers/GetGif.js
+++ src/helpers/GetGif.js
@@ -1,6 +1,6 @@
 export const getGif = async ( category ) => {
     const apiKey = '2beTjWtztNlk4E2OVSwjgmCfo6FO3lGt';
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(img => {
